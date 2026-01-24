c5da5d261b8d05da467b9f1cf60f44da4be1b468
jxxcr <jixxcr@qq.com>
2026-01-24 20:20:16 +0800

    update check logical

--- ,check.sh
+++ ,check.sh
@@ -1,5 +1,11 @@
-term=$1
 separate="==========================================================================================================\n"
+if [ -z "$1" ]
+then
+  path='./'
+else
+  path=$1
+fi
+echo $path
 
 if [ -f "cp2k.slurm" ]
 then
@@ -7,87 +13,87 @@ then
 fi
 
 #if [[ $term == 'neb' ]]
-if find . -maxdepth 1 -name "*BAND*" -print -quit | grep -q . && [ -f "cp2k.slurm" ]
+if find $path -maxdepth 1 -name "*BAND*" -print -quit | grep -q . && [ -f "cp2k.slurm" ]
 then
-  number=`grep NUMBER_OF_REPLICA input.inp | awk '{print $2}' | tr -d '\r'`
+  number=`grep NUMBER_OF_REPLICA $path/input.inp | awk '{print $2}' | tr -d '\r'`
   st=`seq -w 1 $number`
   mapfile -t s <<< "$st"
 
-  if [ -f "$prefix-BAND${s[0]}.out" ]
+  if [ -f "$path/$prefix-BAND${s[0]}.out" ]
   then
     tail $prefix-BAND*.out
     echo -e $separate
-    grep --color=always "\[ .*\]" $prefix-BAND${s[0]}.out | tail -4
+    grep --color=always "\[ .*\]" $path/$prefix-BAND${s[0]}.out | tail -4
     echo -e $separate
-    grep --color=always "Step  Nr" $prefix-BAND${s[0]}.out | tail -1
+    grep --color=always "Step  Nr" $path/$prefix-BAND${s[0]}.out | tail -1
     echo -e $separate
   fi
 
-  if [ -f "$prefix-pos-Replica_nr_${s[0]}-1.xyz" ]
+  if [ -f "$path/$prefix-pos-Replica_nr_${s[0]}-1.xyz" ]
   then
-    anumber=`head -n 1 $prefix-pos-Replica_nr_${s[0]}-1.xyz | awk '{print $1+2}'`
+    anumber=`head -n 1 $path/$prefix-pos-Replica_nr_${s[0]}-1.xyz | awk '{print $1+2}'`
 
-    >path1.xyz; for i in ${s[@]}; do head -n $anumber $prefix-pos-Replica_nr_$i-1.xyz>>path1.xyz; done; ,rp path1.xyz
-    >path2.xyz; for i in ${s[@]}; do tail -n $anumber $prefix-pos-Replica_nr_$i-1.xyz>>path2.xyz; done; ,rp path2.xyz
+    >$path/path1.xyz; for i in ${s[@]}; do head -n $anumber $path/$prefix-pos-Replica_nr_$i-1.xyz>>$path/path1.xyz; done; ,rp $path/path1.xyz
+    >$path/path2.xyz; for i in ${s[@]}; do tail -n $anumber $path/$prefix-pos-Replica_nr_$i-1.xyz>>$path/path2.xyz; done; ,rp $path/path2.xyz
     echo -e $separate
 
-    > ene.dat
-    enei=`grep ENERGY $prefix-BAND${s[0]}.out| tail -1| awk '{print $9}'`
+    > $path/ene.dat
+    enei=`grep ENERGY $path/$prefix-BAND${s[0]}.out| tail -1| awk '{print $9}'`
     for i in ${s[@]}
     do
-      ene=`grep ENERGY $prefix-BAND$i.out| tail -1| awk '{print $9}'`
+      ene=`grep ENERGY $path/$prefix-BAND$i.out| tail -1| awk '{print $9}'`
 
       enec=`python -c "print(($ene - $enei)*27.2114)"`
-      echo -e "$i\t$ene\t$enec" >> ene.dat
+      echo -e "$i\t$ene\t$enec" >> $path/ene.dat
     done
 
-    cat ene.dat
+    cat $path/ene.dat
     echo -e $separate
   fi
 
 else
-  if [ -f "cp2k.slurm" ]
+  if [ -f "$path/cp2k.slurm" ]
   then
-    if [[ -e "./${prefix}-1.ener" ]]
+    if [[ -e "$path/${prefix}-1.ener" ]]
     then
-      tail out.out; tail ${prefix}-1.ener
+      tail $path/out.out; tail $path/${prefix}-1.ener
       echo -e $separate
-      ,rp ${prefix}-pos-1.xyz
+      ,rp $path/${prefix}-pos-1.xyz
       echo -e $separate
     else
-      tail out.out
+      tail $path/out.out
       echo -e $separate
-      grep --color=always STEP out.out| tail
+      grep --color=always STEP $path/out.out| tail
       echo -e $separate
-      grep --color=always "Used time" out.out | tail
+      grep --color=always "Used time" $path/out.out | tail
       echo -e $separate
-      grep --color=always "Maximum step size        " out.out | tail -1
-      grep --color=always "Convergence limit for maximum step size" out.out | tail -1
-      grep --color=always "Maximum gradient        " out.out | tail -1
-      grep --color=always "Convergence limit for maximum gradient" out.out | tail -1
+      grep --color=always "Maximum step size        " $path/out.out | tail -1
+      grep --color=always "Convergence limit for maximum step size" $path/out.out | tail -1
+      grep --color=always "Maximum gradient        " $path/out.out | tail -1
+      grep --color=always "Convergence limit for maximum gradient" $path/out.out | tail -1
       echo -e $separate
     fi
-  elif [ -f "OUTCAR" ]
+  elif [ -f "$path/OUTCAR" ]
   then
-    tail OUTCAR
+    tail $path/OUTCAR
     echo -e $separate
-    tail OSZICAR
+    tail $path/OSZICAR
     echo -e $separate
-  elif [ -f "lammps.slurm" ]
+  elif [ -f "$path/lammps.slurm" ]
   then
-    tail out.out
+    tail $path/out.out
     echo -e $separate
-    tail fp.dat
+    tail $path/fp.dat
     echo -e $separate
-  elif [ -f "abinit.slurm" ]
+  elif [ -f "$path/abinit.slurm" ]
   then
-    grep --color=always "max grad" out.out | tail
+    grep --color=always "max grad" $path/out.out | tail
     echo -e $separate
-    grep --color=always "wall_time" out.out | tail
+    grep --color=always "wall_time" $path/out.out | tail
     echo -e $separate
-    grep --color=always "converged" out.out | tail
+    grep --color=always "converged" $path/out.out | tail
     echo -e $separate
-    grep --color=always "ETOT" out.out | tail
+    grep --color=always "ETOT" $path/out.out | tail
     echo -e $separate
   fi
 fi
